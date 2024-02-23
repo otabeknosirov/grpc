f9ce3c3320739b202df3a8b6a659713c090b9031
otabek94_30 <otabek94_30@mail.ru>
2024-02-23 19:09:44 +0500

    server side streaming is done

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/grpc"
 	"grpc/generated-proto/order"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -32,21 +33,29 @@ type Order struct {
 type Server struct {
 }
 
+// manually-filled map database
 var mapDB map[string]Order = map[string]Order{
-	"perfume": {
+	"perfume-rose": {
 		Id:          "1",
-		Items:       []string{"soup", "shampoo", "cream", "body cream", "cologne"},
+		Items:       []string{"soup", "shampoo", "cream", "body cream", "cologne", "varnish", "hair dye"},
 		Description: "Perfume",
 		Price:       45000000,
 	},
 	"food": {
 		Id:          "2",
-		Items:       []string{"hot-dog", "hamburger", "cheeseburger"},
+		Items:       []string{"hot-dog", "hamburger", "cheeseburger", "chicken", "lavish"},
 		Description: "Fast Food",
 		Price:       2300000,
 	},
+	"perfume-kudrat": {
+		Id:          "1",
+		Items:       []string{"soup", "shampoo", "cream", "body cream", "cologne", "varnish", "hair dye", "ear sticker"},
+		Description: "Perfume",
+		Price:       47800000,
+	},
 }
 
+// GetOrder unary
 func (s *Server) GetOrder(ctx context.Context, ProductId *order.ProductId) (*order.Order, error) {
 	if mapDB == nil {
 		return nil, errors.New("server error")
@@ -61,6 +70,26 @@ func (s *Server) GetOrder(ctx context.Context, ProductId *order.ProductId) (*ord
 		Description: or.Description,
 		Price:       or.Price,
 	}, nil
+}
+
+func (s *Server) GetOrderItems(itemType *order.Item, server order.OrderManagement_GetOrderItemsServer) error {
+	for _, val := range mapDB {
+		for i := range val.Items {
+			item := val.Items[i]
+			if strings.Contains(item, itemType.GetItem()) {
+				if err := server.Send(&order.Order{
+					Id:          val.Id,
+					Items:       val.Items,
+					Description: val.Description,
+					Price:       val.Price,
+				}); err != nil {
+					return err
+				}
+				break
+			}
+		}
+	}
+	return nil
 }
 
 func (s *Server) mustEmbedUnimplementedOrderManagementServer() {
